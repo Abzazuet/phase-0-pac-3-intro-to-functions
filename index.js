804075d2d6b335318a19ace9604626cb0d37d524
Abel Zazueta <abel.zazueta@gmail.com>
2022-03-07 18:24:58 -0800

    Added functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Follow along with the examples here
+function doNothing(){
+}
+function sayHelloTo(firstName) {
+	console.log(`Hello, ${firstName}!`);
+}
+function say(greeting, firstName){
+	console.log("firstName: ",firstName);
+	console.log("greeting: ",greeting);
+	console.log(`${greeting}, ${firstName}!`);
+}
+function add(x, y){
+	return x+y;
+}
+sayHelloTo("Julia");
+sayHelloTo("Abel");
+sayHelloTo("Tere");
+say("'Sup","Abel");
+console.log(add(5, 5));
